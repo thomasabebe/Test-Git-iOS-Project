2a7cd067ae5f4a4ff4c7833e4771fe3e20819532
Thomas Abebe <thomasabebe@192.168.1.4>
2014-06-10 15:40:48 -0400

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Thomas Abebe on 6/10/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Thomas Abebe on 6/10/14.
+//
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
